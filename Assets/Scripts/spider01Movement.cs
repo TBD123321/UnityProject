65ab93be415cc0d20cbad676e708752545cfb8b3
SirMedvedkOo <xxxibearxxx@gmail.com>
2017-07-21 22:23:05 +0300

    1111
    1111

--- Assets/Scripts/spider01Movement.cs
+++ Assets/Scripts/spider01Movement.cs
@@ -6,9 +6,9 @@ public class spider01Movement : MonoBehaviour {
 	
 	private GameObject player;
 	private Animator anim;
-	private bool moveUp, detectedForMove = false, detectedForAttack = false;
-    private enum LastMove {MoveLeft,MoveDown,MoveRight,MoveUp};
-	private LastMove lastMove;
+	private bool detectedForMove, detectedForAttack, isMoving;
+    //private float moveX, moveY, lastMoveX, lastMoveY;
+    private Vector2 move, lastMove;
 
 	public float speed;
 	public int spiderDamage;
@@ -26,11 +26,19 @@ public class spider01Movement : MonoBehaviour {
 			Vector2 spiderPos = transform.position;
 			Vector2 playerPos = player.transform.position;
 			CalculateAngleForAnimMove(spiderPos, playerPos);
-		}
+            anim.SetFloat("MoveX", move.x);
+            anim.SetFloat("MoveY", move.y);
+            anim.SetFloat("LastMoveX", lastMove.x);
+            anim.SetFloat("LastMoveY", lastMove.y);
+        }
         if (detectedForAttack) {
             Vector2 spiderPos = transform.position;
             Vector2 playerPos = player.transform.position;
             CalculateAngleForAnimIdle(spiderPos, playerPos);
+            anim.SetFloat("MoveX", move.x);
+            anim.SetFloat("MoveY", move.y);
+            anim.SetFloat("LastMoveX", lastMove.x);
+            anim.SetFloat("LastMoveY", lastMove.y);
         }
 	}
 
@@ -38,53 +46,87 @@ public class spider01Movement : MonoBehaviour {
 	{
 		float angleBetween = AngleBetweenVector2 (slimePos, playerPos);
 
-		anim.ResetTrigger ("MoveLeft");
-		anim.ResetTrigger ("MoveDown");
-		anim.ResetTrigger ("MoveRight");
-		anim.ResetTrigger ("MoveUp");
+		//anim.ResetTrigger ("MoveLeft");
+		//anim.ResetTrigger ("MoveDown");
+		//anim.ResetTrigger ("MoveRight");
+		//anim.ResetTrigger ("MoveUp");
 
 		if (angleBetween >= 45 && angleBetween < 135)
 		{
-			anim.SetTrigger ("MoveUp");
-
-			lastMove = LastMove.MoveUp;
+            //anim.SetTrigger ("MoveUp");
+            //lastMove = LastMove.MoveUp;
+            move = new Vector2(0f, 1f);
+            lastMove = new Vector2(0f, 1f);
+            //moveY = 1f;
+            //lastMoveY = 1f;
+            //anim.SetFloat("MoveY", moveY);
 		}
 		else if (angleBetween >= 135 || angleBetween < -135)
 		{
-			anim.SetTrigger ("MoveLeft");
-			lastMove = LastMove.MoveLeft;
-		}
+            //anim.SetTrigger ("MoveLeft");
+            //lastMove = LastMove.MoveLeft;
+            move = new Vector2(-1f, 0f);
+            lastMove = new Vector2(-1f, 0f);
+            //moveX = -1f;
+            //lastMoveX = -1f;
+            //anim.SetFloat("MoveX", moveX);
+        }
 		else if (angleBetween >= -135 && angleBetween < -45)
 		{
-			anim.SetTrigger ("MoveDown");
-			lastMove = LastMove.MoveDown;
-		}
+            //anim.SetTrigger ("MoveDown");
+            //lastMove = LastMove.MoveDown;
+            move = new Vector2(0f, -1f);
+            lastMove = new Vector2(0f, -1f);
+            //moveY = -1f;
+            //lastMoveY = -1f;
+            //anim.SetFloat("MoveY", moveY);
+        }
 		else if (angleBetween >= -45 && angleBetween < 45)
 		{
-			anim.SetTrigger ("MoveRight");
-			lastMove = LastMove.MoveRight;
-		}
+            //anim.SetTrigger ("MoveRight");
+            //lastMove = LastMove.MoveRight;
+            move = new Vector2(1f, 0f);
+            lastMove = new Vector2(1f, 0f);
+            //moveX = 1f;
+            //lastMoveX = 1f;
+            //anim.SetFloat("MoveX", moveX);
+        }
 	}
 
     private void CalculateAngleForAnimIdle(Vector2 slimePos, Vector2 playerPos) {
         float angleBetween = AngleBetweenVector2(slimePos, playerPos);
 
-        if (angleBetween >= 45 && angleBetween < 135) {
-            anim.SetTrigger("IdleUp");
+        //anim.ResetTrigger("IdleLeft");
+        //anim.ResetTrigger("IdleDown");
+        //anim.ResetTrigger("IdleRight");
+        //anim.ResetTrigger("IdleUp");
 
-            lastMove = LastMove.MoveUp;
+        if (angleBetween >= 45 && angleBetween < 135) {
+            //anim.SetTrigger("IdleUp");
+            //lastMove = LastMove.MoveUp;
+            lastMove = new Vector2(0f, 1f);
+            //anim.SetFloat("LastMoveY", lastMoveY);
         }
         else if (angleBetween >= 135 || angleBetween < -135) {
-            anim.SetTrigger("IdleLeft");
-            lastMove = LastMove.MoveLeft;
+            //anim.SetTrigger("IdleLeft");
+            //lastMove = LastMove.MoveLeft;
+            lastMove = new Vector2(-1f, 0f);
+            //lastMoveX = -1f;
+            //anim.SetFloat("LastMoveX", lastMoveX);
         }
         else if (angleBetween >= -135 && angleBetween < -45) {
-            anim.SetTrigger("IdleDown");
-            lastMove = LastMove.MoveDown;
+            //anim.SetTrigger("IdleDown");
+            //lastMove = LastMove.MoveDown;
+            lastMove = new Vector2(0f, -1f);
+            //lastMoveY = -1f;
+            //anim.SetFloat("LastMoveY", lastMoveY);
         }
         else if (angleBetween >= -45 && angleBetween < 45) {
-            anim.SetTrigger("IdleRight");
-            lastMove = LastMove.MoveRight;
+            //anim.SetTrigger("IdleRight");
+            //lastMove = LastMove.MoveRight;
+            lastMove = new Vector2(1f, 0f);
+            //lastMoveX = 1f;
+            //anim.SetFloat("LastMoveX", lastMoveX);
         }
 
     }
@@ -98,36 +140,40 @@ public class spider01Movement : MonoBehaviour {
 
 	private void DetectedPlayerForMove()
 	{
-		anim.ResetTrigger ("IdleDown");
-		anim.ResetTrigger ("IdleUp");
-		anim.ResetTrigger ("IdleLeft");
-		anim.ResetTrigger ("IdleRight");
+        //anim.ResetTrigger ("IdleDown");
+        //anim.ResetTrigger ("IdleUp");
+        //anim.ResetTrigger ("IdleLeft");
+        //anim.ResetTrigger ("IdleRight");
+        anim.SetTrigger("IsMoving");
 		detectedForMove = true;
 	}
 	private void UndetectedPlayerForMove()
 	{
 		detectedForMove = false;
-		anim.ResetTrigger ("MoveLeft");
-		anim.ResetTrigger ("MoveDown");
-		anim.ResetTrigger ("MoveRight");
-		anim.ResetTrigger ("MoveUp");
-		if(lastMove == LastMove.MoveDown)
-		{
-			anim.SetTrigger ("IdleDown");
-		}
-		if(lastMove == LastMove.MoveUp)
-		{
-			anim.SetTrigger ("IdleUp");
-		}
-		if(lastMove == LastMove.MoveLeft)
-		{
-			anim.SetTrigger ("IdleLeft");
-		}
-		if(lastMove == LastMove.MoveRight)
-		{
-			anim.SetTrigger ("IdleRight");
-		}
-	}
+        //anim.ResetTrigger ("MoveLeft");
+        //anim.ResetTrigger ("MoveDown");
+        //anim.ResetTrigger ("MoveRight");
+        //anim.ResetTrigger ("MoveUp");
+        anim.ResetTrigger("IsMoving");
+        //if(lastMove.y == 1f)
+        //{
+        //	anim.SetTrigger ("IdleDown");
+        //}
+        //if(lastMove.x == -1f)
+        //{
+        //	anim.SetTrigger ("IdleUp");
+        //}
+        //if(lastMove.y == -1f)
+        //{
+        //	anim.SetTrigger ("IdleLeft");
+        //}
+        //if(lastMove.x == 1f)
+        //{
+        //	anim.SetTrigger ("IdleRight");
+        //}
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
 
     private void DetectedPlayerForAttack() {
         UndetectedPlayerForMove();
