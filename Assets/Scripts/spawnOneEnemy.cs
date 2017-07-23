e50de7bf4f60a1e36a4d6001ad70155a59c1c532
DxDBlackRock <dxd.blackrock@yandex.ru>
2017-07-23 18:51:08 +0300

    spawn point

---
+++ Assets/Scripts/spawnOneEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnOneEnemy : MonoBehaviour {
+
+    public GameObject enemy;
+    public float spawnSpeed;
+
+    private bool canSpawn = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canSpawn)
+        {
+            Instantiate(enemy);
+            StartCoroutine(CanSpawn());
+        }
+    }
+
+    IEnumerator CanSpawn()
+    {
+        canSpawn = false;
+        yield return new WaitForSeconds(spawnSpeed);
+        canSpawn = true;
+    }
+}
