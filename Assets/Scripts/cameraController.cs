50ffa9fc7ef4b43ab8c704a47d45697d5d6cb232
SirMedvedkOo <xxxibearxxx@gmail.com>
2017-07-26 20:10:05 +0300

    camera4

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -23,7 +23,7 @@ public class cameraController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void Update() {
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
     }
